79e74ceaaf18bcab879e14839baf50f39897498d
Pra1re <168174845+Pra1re@users.noreply.github.com>
2024-05-04 16:18:24 +0600

    Update test1.c

--- test1.c
+++ test1.c
@@ -26,7 +26,6 @@ map *p[table]={NULL};
 
 
 void seperate(char *a){
-    //printf("%s\n",a);
 int i=0;
 
 
@@ -44,15 +43,14 @@ char x[100]="";
     }
 if(a[i]==' '){
     i++;}
-    //printf("%s\n",x);
     int f=hashfunc(x);
-    //printf("%d\n",f);
+    
     if(p[f]==NULL){
 
 p[f]=malloc(sizeof(map));
 
         strcpy(p[f]->word,x);
-        //printf("%s ",p[f]->word);
+        
         p[f]->ptr=NULL;
     }
     else{
@@ -63,7 +61,7 @@ while(temp->ptr!=NULL){
 }
 map *neow=malloc(sizeof(map));
 strcpy(neow->word,x);
-//printf("%s ",neow->word);
+
 neow->ptr=NULL;
 temp->ptr=neow;
     }
@@ -79,7 +77,7 @@ temp->ptr=neow;
 }
 
 int lookup(char *b){
-//printf("%s\n",b);
+
 int i=0,result=0,totalwords=0;
 while(b[i]!='\0'){
 char x[100]="";
@@ -96,23 +94,20 @@ char x[100]="";
     totalwords++;
     if(b[i]==' '){
     i++;}
-//printf("%s ",x);
+
     int f=hashfunc(x);
-    //printf("f = %d ",f);
+    
     map *temp=p[f];
-    //printf("x = %s temp word = %s\n",x,p[f]->word);
+   
     if(p[f]==NULL){
         result=0;
         break;
     }
-    /*else if(strcmp(temp->word,x)==0){
-            result=1;
-                strcpy(temp->word,"youdonemessedupa-aron");
-                }*/
-    //temp=p[f];
+
+        
+
     else{
     while(temp!=NULL){
-    //printf("comparison between x and temp word %d\n",p);
         if(strcmp(temp->word,x)==0){
                 result=1;
                 strcpy(temp->word,"youdonemessedupa-aron");
@@ -134,9 +129,6 @@ if(result==0){
 
 
 }
-//printf("result %d\n",result);
-//printf("total words %d\n",totalwords);
-
 return result;
 
 
@@ -150,30 +142,20 @@ return result;
 
 int main(){
 int m, n;
-    //printf("Enter the lengths of the strings: ");
-    scanf("%d", &m);
-scanf("%d",&n);
+    scanf("%d%d", &m,&n);
     while (getchar() != '\n');
-
     char a[30005];
 
-    //printf("Enter the first string: ");
     fgets(a, 30005, stdin);
     int f = strlen(a);
         a[f - 1] = '\0';
     seperate(a);
-    //while (getchar() != '\n');
     char b[30005];
 
-    //printf("Enter the second string: ");
     fgets(b, 30005, stdin);
     int g = strlen(b);
         b[g - 1] = '\0';
     int i=lookup(b);
-//printf("%d",i);
-
-
-
 
 if(i==1){
     printf("Yes");
